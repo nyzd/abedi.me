9dd3ba4eaf6f787fddfa1b1f4a3b55f2c38d4c31
AzerothA <telifesite@gmail.com>
2023-11-08 00:07:02 +0330

    new update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "../components/theme-provider";
 //import { Button } from "../components/ui/button";
 import { Separator } from "../components/ui/separator";
 import "./globals.css";
-import Menu from "./menu";
+//import Menu from "./menu";
 
 export default function RootLayout({ children }: any) {
     return (
@@ -11,14 +11,10 @@ export default function RootLayout({ children }: any) {
                 <head />
                 <body>
                     <ThemeProvider attribute="class" defaultTheme="dark">
-                        <header className="flex flex-row px-40 p-3 items-center">
-                            <div className="mr-auto flex flex-row items-center space-x-4 text-sm">
-                                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-                                    Abedi.me
-                                </h3>
-                                <Separator orientation="vertical" />
-                                <Menu />
-                            </div>
+                        <header className="flex flex-row justify-center p-3 items-center">
+                            <h3 className="text-2xl font-semibold tracking-tight">
+                                Abedi.me
+                            </h3>
                         </header>
                         <Separator />
                         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,45 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../components/ui/card";
+
 export const runtime = "edge";
 
-export default function Home() {
+const FILES_LIST_ROUTE: string = "/git/trees/main?recursive=1";
+
+async function getPosts(): Promise<object> {
+    const response = await fetch(
+        `${process.env.GITHUB_API_URL}/repos/0xYakuza/${process.env.POSTS_REPO_NAME}${FILES_LIST_ROUTE}`
+    );
+
+    return response.json();
+}
+
+const cutName = (path: string): string => path.split('-')[0];
+
+const cutExtension = (path: string): string =>
+    path.split('.')[0];
+
+const cutDescription = (path: string): string => path.split('-')[1];
+
+export default async function Home() {
+    const posts: any = await getPosts();
+
     return (
-        <main>
+        <main className="flex justify-center">
+            <div className="p-5 w-1/3">
+                {(posts.tree as any[]).map((post, index) => (
+                    <Card className="cursor-pointer" key={index}>
+                        <CardHeader>
+                            <CardTitle>{cutName(cutExtension(post.path))}</CardTitle>
+                            <CardDescription>{cutExtension(cutDescription(post.path))}</CardDescription>
+                        </CardHeader>
+                    </Card>))}
+            </div>
         </main>
     )
 }
