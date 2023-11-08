52622ac76efa6381eca4ab16909b9e5fe92d3d1e
AzerothA <telifesite@gmail.com>
2023-11-08 15:19:04 +0330

    new update

---
+++ src/app/(components)/card.tsx
@@ -0,0 +1,14 @@
+import 
+
+export function Card({ children }: any) {
+    return (<div></div>);
+}
+
+export function CardHead({ children }: any) {
+    return (<div>{children}</div>);
+}
+
+export function CardTitle({ children }: any) {
+    return (<div>{children}</div>);
+}
+
---
+++ src/app/(components)/header.tsx
@@ -0,0 +1,9 @@
+import styles from "./header.module.css";
+
+export default function Header() {
+    return (
+        <div className={styles.header}>
+            <h1>Abedi.me</h1>
+        </div>
+    )
+}
